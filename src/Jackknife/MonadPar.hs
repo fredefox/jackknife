1d6b25da40a54fd2bb16b7155046efa2ba265e45
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-03-29 11:10:48 +0200

    Implement Eval and MonadPar versions

--- src/Jackknife/MonadPar.hs
+++ src/Jackknife/MonadPar.hs
@@ -1,4 +1,27 @@
 module Jackknife.MonadPar (jackknife) where
 
-jackknife :: ([a] -> b) -> [a] -> [b]
-jackknife = error "Unimplemented"
+import           Control.Monad.Par        (runPar, NFData, Par)
+import qualified Control.Monad.Par as Par (parMap)
+
+import Jackknife (resamples)
+
+granularity :: Int
+granularity = 50
+
+jackknife :: NFData b => ([a] -> b) -> [a] -> [b]
+jackknife f = runPar . parMapChunked granularity f . resamples 500
+
+-- | Restricts the type of `Par.parMap` to avoid ambiguity.
+parMap :: NFData b => (a -> b) -> [a] -> Par [b]
+parMap = Par.parMap
+
+-- | Parallel map with granularity control. It performs the map on parts of the
+-- list of the specified size.
+parMapChunked :: NFData b => Int -> (a -> b) -> [a] -> Par [b]
+parMapChunked threshold f xs = concat <$> parMap (map f) (chunksOf threshold xs)
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = l : chunksOf n r
+  where
+    (l, r) = splitAt n xs
