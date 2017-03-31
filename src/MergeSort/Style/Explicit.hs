8d36f865fd2a20f86e2da1d2a8b8a25c38b59f0e
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-03-31 11:52:59 +0200

    Add granularity control to explicit parallel mergesort

--- src/MergeSort/Style/Explicit.hs
+++ src/MergeSort/Style/Explicit.hs
@@ -1,17 +1,29 @@
-module MergeSort.Style.Explicit (mergesort, merge, halve) where
+module MergeSort.Style.Explicit (mergesort) where
 
-import Control.DeepSeq (NFData, rnf)
 import Control.Parallel
 
-import MergeSort.Util
+import MergeSort.Util (halve, merge)
+import qualified MergeSort.Style.Sequential as Sequential
 
--- TODO: Implement granularity control
--- | Explicit parallel implementation
-mergesort :: (NFData a, Ord a) => [a] -> [a]
-mergesort [] = []
-mergesort [x] = [x]
-mergesort xs = rnf l' `par` rnf r' `pseq` (l' `merge` r')
-  where
-    (l, r) = halve xs
-    l' = mergesort l
-    r' = mergesort r
+granularity :: Int
+granularity = 5
+
+mergesort :: Ord a => [a] -> [a]
+mergesort = granularMergesort granularity
+
+-- | Explicit parallel implementation with granularity control.
+granularMergesort :: Ord a => Int -> [a] -> [a]
+granularMergesort n
+  | n <= 0    = Sequential.mergesort
+  | otherwise = parMergesort
+    where
+      step :: Ord a => [a] -> [a]
+      step = granularMergesort (n - 1)
+      parMergesort :: Ord a => [a] -> [a]
+      parMergesort [] = []
+      parMergesort [x] = [x]
+      parMergesort xs = l' `par` r' `pseq` (l' `merge` r')
+        where
+          (l, r) = halve xs
+          l' = step l
+          r' = step r
