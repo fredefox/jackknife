c1b314d4f566de55f49f29feec40b270707cf4bb
Liam <hughes@student.chalmers.se>
2017-04-01 16:11:28 +0200

    Implem'd alternate explicit mergesort. Achieved only small speedups

--- app/MergeSort.hs
+++ app/MergeSort.hs
@@ -7,6 +7,7 @@ import Control.Parallel
 
 import qualified MergeSort.Style.Sequential as Sequential (mergesort)
 import qualified MergeSort.Style.Explicit   as Explicit   (mergesort)
+import qualified AltMS
 -- TODO Implement some of these:
 -- import qualified MergeSort.Style.Eval       as Eval       (mergesort)
 -- import qualified MergeSort.Style.Strat      as Strat      (mergesort)
@@ -15,7 +16,9 @@ import qualified MergeSort.Style.Explicit   as Explicit   (mergesort)
 doBenchmark :: [Float] -> IO ()
 doBenchmark rs =
   defaultMain
-    [ bench "[sequential] mergesort" (nf Sequential.mergesort rs)
+    [ bench "altms sequential" $ nf AltMS.mergesort rs
+    , bench "altms explicit" $ nf (AltMS.pmergesort 2) rs  
+    , bench "[sequential] mergesort" (nf Sequential.mergesort rs)
     , bench "[explicit]   mergesort" (nf Explicit.mergesort   rs)
     -- , bench "[eval]       mergesort" (nf Eval.mergesort       rs)
     -- , bench "[strat]      mergesort" (nf Strat.mergesort      rs)
@@ -23,11 +26,11 @@ doBenchmark rs =
     ]
 
 aList :: [Float]
-aList = take 10000000 $ randoms (mkStdGen 1337)
+aList = take 100000 $ randoms (mkStdGen 1337)
 
 main :: IO ()
--- main = doBenchmark aList
-main = evaluate . Explicit.mergesort $ aList
+main = doBenchmark aList
+--main = evaluate . Explicit.mergesort $ aList
 
 evaluate :: NFData a => a -> IO ()
 evaluate x = rnf x `pseq` return ()
