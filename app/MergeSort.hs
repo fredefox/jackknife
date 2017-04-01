1dd9c994603a4431bd9c5d58cafa546d8cc08f28
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-04-01 13:17:03 +0200

    Implment eval-style for mergesort

--- app/MergeSort.hs
+++ app/MergeSort.hs
@@ -8,7 +8,7 @@ import Control.Parallel
 import qualified MergeSort.Style.Sequential as Sequential (mergesort)
 import qualified MergeSort.Style.Explicit   as Explicit   (mergesort)
 -- TODO Implement some of these:
--- import qualified MergeSort.Style.Eval       as Eval       (mergesort)
+import qualified MergeSort.Style.Eval       as Eval       (mergesort)
 -- import qualified MergeSort.Style.Strat      as Strat      (mergesort)
 -- import qualified MergeSort.Style.MonadPar   as MonadPar   (mergesort)
 
@@ -17,17 +17,17 @@ doBenchmark rs =
   defaultMain
     [ bench "[sequential] mergesort" (nf Sequential.mergesort rs)
     , bench "[explicit]   mergesort" (nf Explicit.mergesort   rs)
-    -- , bench "[eval]       mergesort" (nf Eval.mergesort       rs)
+    , bench "[eval]       mergesort" (nf Eval.mergesort       rs)
     -- , bench "[strat]      mergesort" (nf Strat.mergesort      rs)
     -- , bench "[monad-par]  mergesort" (nf MonadPar.mergesort   rs)
     ]
 
 aList :: [Float]
-aList = take 10000000 $ randoms (mkStdGen 1337)
+aList = take 1000000 $ randoms (mkStdGen 1337)
 
 main :: IO ()
 -- main = doBenchmark aList
-main = evaluate . Explicit.mergesort $ aList
+main = evaluate . Eval.mergesort $ aList
 
 evaluate :: NFData a => a -> IO ()
 evaluate x = rnf x `pseq` return ()
